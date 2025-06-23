e77d4221237c87534cb2be0c0a8607a0609ba0cb
Stellar-security-os <150491881+stellar-security-os@users.noreply.github.com>
2025-06-23 22:11:48 +0200

    Added API.php

--- app/Http/Controllers/V1/LoginController.php
+++ app/Http/Controllers/V1/LoginController.php
@@ -2,9 +2,51 @@
 
 namespace App\Http\Controllers\V1;
 
-use app\Http\Controllers\Controller;
+use App\Http\Controllers\Controller;
+use App\Services\UserService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
+
+    public function __construct(public UserService $userService)
+    {}
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function auth(Request $request): JsonResponse
+    {
+
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+        $auth = $this->userService->auth([
+            'username' => $username,
+            'password' => $password
+        ])->object();
+
+        return response()->json($auth);
+
+    }
+
+    public function create(Request $request): JsonResponse
+    {
+
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+        $auth = $this->userService->create([
+            'username' => $username,
+            'password' => $password
+        ])->object();
+
+
+
+        return response()->json($auth);
+
+    }
 
 }
