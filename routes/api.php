329eb2a9e46827afa99ddf5bcbe9cf2045d11549
Stellar-security-os <150491881+stellar-security-os@users.noreply.github.com>
2025-06-24 01:26:58 +0200

    Added LoginController routes

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,19 @@
 <?php
 
+use App\Http\Controllers\V1\LoginController;
 use App\Http\Controllers\V1\NotesController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('v1')->group(function () {
 
+    Route::prefix('logincontroller')->group(function () {
+        Route::post('/create', [LoginController::class, 'create']);
+        Route::post('/auth', [LoginController::class, 'auth']);
+    });
+
+
     Route::prefix('notescontroller')->group(function () {
         Route::post('/update', [NotesController::class, 'update']);
     });
-
-    Route::prefix(' logincontroller')->group(function () {
-        Route::controller(\App\Http\Controllers\V1\LoginController::class)->group(function () {
-            Route::post('/login', 'auth');
-            Route::post('/create', 'create');
-            Route::post('/sendresetpasswordlink', 'sendresetpasswordlink');
-        });
-    });
-
 });
