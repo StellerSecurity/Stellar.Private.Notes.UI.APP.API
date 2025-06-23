a6c2eea8ae903b4d111f09c6ab6427d352ee8bf3
Stellar-security-os <150491881+stellar-security-os@users.noreply.github.com>
2025-06-23 22:09:03 +0200

    Added API.php

---
+++ app/Http/Controllers/V1/LoginController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use app\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+
+}
---
+++ app/Http/Controllers/V1/NotesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use app\Http\Controllers\Controller;
+
+class NotesController extends Controller
+{
+
+    public function index()
+    {
+
+    }
+
+}
