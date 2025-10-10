a8d40f05dbee13b52ff3555a5c7cd7699c2470d5
stellar-security-os <stellarsecaps@gmail.com>
2025-10-10 12:09:17 +0200

    Changed to post endpoints.

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::prefix('v1')->group(function () {
     Route::prefix('notescontroller')->group(function () {
         Route::post('/upload', [NotesController::class, 'upload']);
         Route::post('/sync-plan', [NotesController::class, 'sync']);
-        Route::get('/download', [NotesController::class, 'download']);
-        Route::get('/find', [NotesController::class, 'find']);
+        Route::post('/download', [NotesController::class, 'download']);
+        Route::post('/find', [NotesController::class, 'find']);
     });
 });
